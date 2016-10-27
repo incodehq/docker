7bf06506fdc7d02dbb6fae9e756800a8adb4a629
Hessel Bakker <hbakker@incode.org>
2016-10-27 13:33:15 +0200

    Fixed wrong db reference

--- isis-simpleapp/run.sh
+++ isis-simpleapp/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker run --name postgres -d incodehq/postgres
-docker run --name isis-simpleapp --link postgres:db -e ISIS_OPTS="isis.persistor.datanucleus.impl.javax.jdo.option.ConnectionDriverName=org.postgresql.Driver||isis.persistor.datanucleus.impl.javax.jdo.option.ConnectionURL=jdbc:postgresql://db:5432/incodehqdb||isis.persistor.datanucleus.impl.javax.jdo.option.ConnectionUserName=incodehq||isis.persistor.datanucleus.impl.javax.jdo.option.ConnectionPassword=incodehq" -p 8080:8080 -d incodehq/isis-simpleapp
+docker run --name isis-simpleapp --link postgres:db -e ISIS_OPTS="isis.persistor.datanucleus.impl.javax.jdo.option.ConnectionDriverName=org.postgresql.Driver||isis.persistor.datanucleus.impl.javax.jdo.option.ConnectionURL=jdbc:postgresql://db:5432/incodehq||isis.persistor.datanucleus.impl.javax.jdo.option.ConnectionUserName=incodehq||isis.persistor.datanucleus.impl.javax.jdo.option.ConnectionPassword=incodehq" -p 8080:8080 -d incodehq/isis-simpleapp
 # run with in memory database:
 # docker run -d -p 8080:8080 --name isis-simpleapp incodehq/isis-simpleapp
